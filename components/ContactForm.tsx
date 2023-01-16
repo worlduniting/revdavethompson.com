57d65650d27741ad40b1e5222539963f8a15a031
Rev. Dave Thompson <davidandrewthompson@gmail.com>
2023-01-16 14:37:40 -0800

    removed bad type from component tag

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -148,7 +148,6 @@ export default function ContactForm() {
                       <textarea
                         id="insightly_Description"
                         name="Description"
-                        type="text"
                         rows={8}
                         className="form-textarea mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-violet-500 focus:ring-violet-500 sm:text-sm"
                       />
